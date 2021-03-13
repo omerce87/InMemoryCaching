50a4de4e78d5c38a9eab65001cdf39c35c9c48c7
Ömer SAĞLAM <omerce87@gmail.com>
2021-03-13 08:03:23 +0300

    Add project files.

---
+++ MemoryManagement/Controllers/InMemoryController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemoryManagement.Controllers
+{
+    public class InMemoryController : Controller
+    {
+        private IMemoryCache _memorycache;
+        public InMemoryController(IMemoryCache memorycache)
+        {
+            _memorycache = memorycache;
+        }
+
+        public IActionResult Index()
+        {
+            _memorycache.Set<string>("currentTime", DateTime.Now.ToString());
+
+            return View();
+        }
+
+        public IActionResult GetTime()
+        {
+            _memorycache.TryGetValue<string>("currentTime", out string mycurrenttime);
+
+            ViewBag.currenttime = mycurrenttime;
+
+            return View();
+        }
+    }
+}
