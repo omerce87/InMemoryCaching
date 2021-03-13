ea2e4d55ec3cb65e3740c85cc6b8001e24f04ace
Ömer SAĞLAM <omerce87@gmail.com>
2021-03-13 12:42:46 +0300

    Redis Controller Added

---
+++ RedisApp/Controllers/RedisController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisApp.Controllers
+{
+    public class RedisController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Redis Value Types
+            //String
+            //List
+            //Set
+            //Sorted Set
+            //Hash
+            return View();
+        }
+    }
+}
---
+++ RedisApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RedisApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
