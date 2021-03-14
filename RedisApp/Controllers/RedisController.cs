ea56614b1dc90e7d1a998322f45de97ee8705c06
Ömer SAĞLAM <omerce87@gmail.com>
2021-03-14 15:35:20 +0300

    Added new features for redis integration

--- RedisApp/Controllers/RedisController.cs
+++ RedisApp/Controllers/RedisController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using RedisApp.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,20 +17,66 @@ namespace RedisApp.Controllers
         {
             _distributedCache = distributedCache;
         }
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
 
             DistributedCacheEntryOptions opt = new DistributedCacheEntryOptions();
             opt.AbsoluteExpiration = DateTime.Now.AddSeconds(10);
-            _distributedCache.SetString("namesurname", "Ömer SAĞLAM", opt);
+            // Classic Method
+
+            //_distributedCache.SetString("namesurname", "Ömer SAĞLAM", opt);
+
+            // Asyncronous method
+            await _distributedCache.SetStringAsync("namesurname", "Ömer SAĞLAM", default);
+
+            Customer newcustomer = new Customer() { ID = 1, CustomerName = "Customer1", CustomerDesc = "Customer Description", CreatedDate = DateTime.Now };
+
+            string mycustomer_json = JsonConvert.SerializeObject(newcustomer);
+
+            await _distributedCache.SetStringAsync("mycustomer", mycustomer_json, opt);
 
             return View();
         }
 
-        public IActionResult GetValue()
+        public async Task<IActionResult> GetValue()
         {
-            ViewBag.str = _distributedCache.GetString("namesurname");
+            //ViewBag.str = _distributedCache.GetString("namesurname");
+
+            ViewBag.str = await _distributedCache.GetStringAsync("namesurname");
+
+            string json_customer = await _distributedCache.GetStringAsync("mycustomer").ConfigureAwait(false);
+            if(!String.IsNullOrEmpty(json_customer))
+            { 
+                Customer customer = JsonConvert.DeserializeObject<Customer>(json_customer);
+                if (customer != null)
+                    ViewBag.Customername = customer.CustomerName;
+            }
             return View();
+        }
+
+        public async Task<IActionResult> ClearKey()
+        {
+            //_distributedCache.Remove("namesurname");
+
+            await _distributedCache.RemoveAsync("namesurname");
+            return View();
+        }
+
+        public IActionResult SetImage()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/image1.jpg");
+            byte[] imagebytes = System.IO.File.ReadAllBytes(path);
+            _distributedCache.Set("showimage", imagebytes);
+
+            return View();
+        }
+
+        public IActionResult ShowImage()
+        {
+            byte[] imagebytes = _distributedCache.Get("showimage");
+            
+            return File(imagebytes, "image/jpg");
         }
     }
 }
---
+++ RedisApp/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisApp.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerDesc { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
